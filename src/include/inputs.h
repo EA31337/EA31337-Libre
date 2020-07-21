85faf5e2184dbdfe05582588cb3e988ff243c099
kenorb <kenorb@users.noreply.github.com>
2020-07-21 10:54:49 +0100

    Improves code formatting

--- src/include/inputs.h
+++ src/include/inputs.h
@@ -24,48 +24,50 @@
 //+------------------------------------------------------------------+
 
 // Includes strategies.
-input static string __Strategy_Timeframes__ = "-- Strategy's timeframes --"; // >>> STRATEGY'S TIMEFRAMES (1-255: M1=1,M5=2,M15=4,M30=8,H1=16,H2=32,H4=64...) <<<
-input unsigned int AC_Active_Tf = 0; // AC: Activated timeframes
-input unsigned int AD_Active_Tf = 0; // AD: Activated timeframes
-input unsigned int ADX_Active_Tf = 0; // ADX: Activated timeframes
-input unsigned int Alligator_Active_Tf = 0; // Alligator: Activated timeframes
-input unsigned int ATR_Active_Tf = 0; // ATR: Activated timeframes
-input unsigned int Awesome_Active_Tf = 0; // Awesome: Activated timeframes
-input unsigned int Bands_Active_Tf = 0; // Bands: Activated timeframes
-input unsigned int BearsPower_Active_Tf = 0; // BearsPower: Activated timeframes
-input unsigned int BullsPower_Active_Tf = 0; // BullsPower: Activated timeframes
-input unsigned int BWMFI_Active_Tf = 0; // BWMFI: Activated timeframes
-input unsigned int CCI_Active_Tf = 15; // CCI: Activated timeframes
-input unsigned int DeMarker_Active_Tf = 0; // DeMarker: Activated timeframes
-input unsigned int Envelopes_Active_Tf = 4; // Envelopes: Activated timeframes
-input unsigned int Force_Active_Tf = 0; // Force: Activated timeframes
-input unsigned int Fractals_Active_Tf = 0; // Fractals: Activated timeframes
-input unsigned int Gator_Active_Tf = 0; // Gator: Activated timeframes
-input unsigned int Ichimoku_Active_Tf = 0; // Ichimoku: Activated timeframes
-input unsigned int MA_Active_Tf = 0; // MA: Activated timeframes
-input unsigned int MACD_Active_Tf = 0; // MACD: Activated timeframes
-input unsigned int MFI_Active_Tf = 0; // MFI: Activated timeframes
-input unsigned int Momentum_Active_Tf = 0; // Momentum: Activated timeframes
-input unsigned int OBV_Active_Tf = 0; // OBV: Activated timeframes
-input unsigned int OsMA_Active_Tf = 0; // OsMA: Activated timeframes
-input unsigned int RSI_Active_Tf = 12; // RSI: Activated timeframes
-input unsigned int RVI_Active_Tf = 0; // RVI: Activated timeframes
-input unsigned int SAR_Active_Tf = 8; // SAR: Activated timeframes
-input unsigned int StdDev_Active_Tf = 0; // StdDev: Activated timeframes
-input unsigned int Stochastic_Active_Tf = 0; // Stochastic: Activated timeframes
-input unsigned int WPR_Active_Tf = 0; // WPR: Activated timeframes
-input unsigned int ZigZag_Active_Tf = 0; // ZigZag: Activated timeframes
+input static string __Strategy_Timeframes__ =
+    "-- Strategy's timeframes --";  // >>> STRATEGY'S TIMEFRAMES (1-255: M1=1,M5=2,M15=4,M30=8,H1=16,H2=32,H4=64...) <<<
+input unsigned int AC_Active_Tf = 0;          // AC: Activated timeframes
+input unsigned int AD_Active_Tf = 0;          // AD: Activated timeframes
+input unsigned int ADX_Active_Tf = 0;         // ADX: Activated timeframes
+input unsigned int Alligator_Active_Tf = 0;   // Alligator: Activated timeframes
+input unsigned int ATR_Active_Tf = 0;         // ATR: Activated timeframes
+input unsigned int Awesome_Active_Tf = 0;     // Awesome: Activated timeframes
+input unsigned int Bands_Active_Tf = 0;       // Bands: Activated timeframes
+input unsigned int BearsPower_Active_Tf = 0;  // BearsPower: Activated timeframes
+input unsigned int BullsPower_Active_Tf = 0;  // BullsPower: Activated timeframes
+input unsigned int BWMFI_Active_Tf = 0;       // BWMFI: Activated timeframes
+input unsigned int CCI_Active_Tf = 15;        // CCI: Activated timeframes
+input unsigned int DeMarker_Active_Tf = 0;    // DeMarker: Activated timeframes
+input unsigned int Envelopes_Active_Tf = 4;   // Envelopes: Activated timeframes
+input unsigned int Force_Active_Tf = 0;       // Force: Activated timeframes
+input unsigned int Fractals_Active_Tf = 0;    // Fractals: Activated timeframes
+input unsigned int Gator_Active_Tf = 0;       // Gator: Activated timeframes
+input unsigned int Ichimoku_Active_Tf = 0;    // Ichimoku: Activated timeframes
+input unsigned int MA_Active_Tf = 0;          // MA: Activated timeframes
+input unsigned int MACD_Active_Tf = 0;        // MACD: Activated timeframes
+input unsigned int MFI_Active_Tf = 0;         // MFI: Activated timeframes
+input unsigned int Momentum_Active_Tf = 0;    // Momentum: Activated timeframes
+input unsigned int OBV_Active_Tf = 0;         // OBV: Activated timeframes
+input unsigned int OsMA_Active_Tf = 0;        // OsMA: Activated timeframes
+input unsigned int RSI_Active_Tf = 12;        // RSI: Activated timeframes
+input unsigned int RVI_Active_Tf = 0;         // RVI: Activated timeframes
+input unsigned int SAR_Active_Tf = 8;         // SAR: Activated timeframes
+input unsigned int StdDev_Active_Tf = 0;      // StdDev: Activated timeframes
+input unsigned int Stochastic_Active_Tf = 0;  // Stochastic: Activated timeframes
+input unsigned int WPR_Active_Tf = 0;         // WPR: Activated timeframes
+input unsigned int ZigZag_Active_Tf = 0;      // ZigZag: Activated timeframes
 
-//input string __Trade_Parameters__ = "-- Trade parameters --"; // >>> TRADE <<<
-//input ulong TimeframeFilter = 0; // Timeframes filter (0 - auto)
-//input double MinPipChangeToTrade = 0.4; // Min pip change to trade (0 = every tick)
+// input string __Trade_Parameters__ = "-- Trade parameters --"; // >>> TRADE <<<
+// input ulong TimeframeFilter = 0; // Timeframes filter (0 - auto)
+// input double MinPipChangeToTrade = 0.4; // Min pip change to trade (0 = every tick)
 
-input string __Logging_Parameters__ = "-- Settings for logging & messages --"; // >>> LOGS & MESSAGES <<<
-input bool PrintLogOnChart = true; // Display info on chart
-input ENUM_LOG_LEVEL VerboseLevel = V_INFO; // Level of log verbosity
-input bool WriteSummaryReport = true; // Write summary report on finish
+input string __Logging_Parameters__ = "-- Settings for logging & messages --";  // >>> LOGS & MESSAGES <<<
+input bool PrintLogOnChart = true;                                              // Display info on chart
+input ENUM_LOG_LEVEL VerboseLevel = V_INFO;                                     // Level of log verbosity
+input bool WriteSummaryReport = true;                                           // Write summary report on finish
 
-input string __Other_Parameters__ = "-- Other parameters --"; // >>> OTHER PARAMETERS <<<
-input uint MagicNumber = 31337; // Starting EA magic number
+input string __Other_Parameters__ = "-- Other parameters --";  // >>> OTHER PARAMETERS <<<
+input uint MagicNumber = 31337;                                // Starting EA magic number
 
-input string __EA_Parameters__ = "-- End of input parameters for " + ea_name + " v" + ea_version + " --"; // >>> EA31337 <<<
+input string __EA_Parameters__ =
+    "-- End of input parameters for " + ea_name + " v" + ea_version + " --";  // >>> EA31337 <<<
