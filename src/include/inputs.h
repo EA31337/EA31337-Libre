a107ede2489c66623465a6fa229750e84f92f3a0
kenorb <kenorb@users.noreply.github.com>
2021-09-01 20:18:49 +0100

    Sets timeframe to 24576 (H2+H3)

--- src/include/inputs.h
+++ src/include/inputs.h
@@ -30,7 +30,7 @@ input static string __EA_Strategy__ = "-- Trading strategy --";  // >>> TRADING 
 input group "Trading strategy"
 #endif
 input ENUM_STRATEGY EA_Strategy = STRAT_SAR;  // Strategy
-input uint EA_Strategy_Active_Tf = 16384;     // Timeframes (M1=1,M2=2,M5=16,M15=256,M30=1024,H1=2048,H2,H3,H4,H6,H8)
+input uint EA_Strategy_Active_Tf = 24576;     // Timeframes (M1=1,M2=2,M5=16,M15=256,M30=1024,H1=2048,H2,H3,H4,H6,H8)
 
 #ifdef __MQL4__
 extern string __EA_Risk_Params__ = "-- Risk management --";  // >>> RISK <<<
