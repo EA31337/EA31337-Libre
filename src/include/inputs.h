8823f7d66572e0aa56fd71fcb879deb6b7649813
kenorb <kenorb@users.noreply.github.com>
2022-12-14 16:49:26 +0000

    Changes active timeframes for better results

--- src/include/inputs.h
+++ src/include/inputs.h
@@ -31,7 +31,7 @@ input group "Trading strategy"
 #endif
 input ENUM_STRATEGY EA_Strategy = STRAT_ALLIGATOR;  // Strategy
 input unsigned int EA_Strategy_Active_Tf =
-    M30B + H1B + H2B + H3B + H4B + H6B + H8B;  // Timeframes (M1=1,M2=2,M5=16,M15=256,M30=1024,H1=2048,H2,H3,H4,H6,H8)
+    H1B + H2B + H3B + H4B;  // Timeframes (M1=1,M2=2,M5=16,M15=256,M30=1024,H1=2048,H2,H3,H4,H6,H8)
 
 #ifdef __MQL4__
 extern string __EA_Risk_Params__ = "-- Risk management --";  // >>> RISK <<<
