b837968266a814c7065862a1c5dfa8ab9060104d
kenorb <kenorb@users.noreply.github.com>
2023-09-10 21:17:17 +0100

    Adds new strategies

--- src/include/ea.h
+++ src/include/ea.h
@@ -58,16 +58,6 @@ class EALibre : public EA {
    */
   void OnStrategyAdd(Strategy *_strat) {
     EA::OnStrategyAdd(_strat);
-    switch (_strat.Get<ENUM_STRATEGY>(STRAT_PARAM_TYPE)) {
-      case STRAT_META_MIRROR:
-        // @todo: Move this logic to strategy.
-        ((Stg_Meta_Mirror *)_strat).SetStrategies(THIS_PTR);
-        break;
-      case STRAT_META_MULTI:
-        // @todo: Move this logic to strategy.
-        ((Stg_Meta_Multi *)_strat).SetStrategies(THIS_PTR);
-        break;
-    }
   }
 
   /**
@@ -141,6 +131,8 @@ class EALibre : public EA {
         return StrategyAdd<Stg_ADX>(_tfs, _magic_no, _stg);
       case STRAT_AMA:
         return StrategyAdd<Stg_AMA>(_tfs, _magic_no, _stg);
+      case STRAT_ARROWS:
+        return StrategyAdd<Stg_Arrows>(_tfs, _magic_no, _stg);
       case STRAT_ASI:
         return StrategyAdd<Stg_ASI>(_tfs, _magic_no, _stg);
       case STRAT_ATR:
@@ -181,20 +173,34 @@ class EALibre : public EA {
         return StrategyAdd<Stg_Indicator>(_tfs, _magic_no, _stg);
       case STRAT_MA:
         return StrategyAdd<Stg_MA>(_tfs, _magic_no, _stg);
+      case STRAT_MA_CROSS_PIVOT:
+        return StrategyAdd<Stg_MA_Cross_Pivot>(_tfs, _magic_no, _stg);
+      case STRAT_MA_CROSS_SHIFT:
+        return StrategyAdd<Stg_MA_Cross_Shift>(_tfs, _magic_no, _stg);
+      case STRAT_MA_CROSS_SUP_RES:
+        return StrategyAdd<Stg_MA_Cross_Sup_Res>(_tfs, _magic_no, _stg);
       case STRAT_MACD:
         return StrategyAdd<Stg_MACD>(_tfs, _magic_no, _stg);
-      case STRAT_META_MIRROR:
-        return StrategyAdd<Stg_Meta_Mirror>(_tfs, _magic_no, _stg);
-      case STRAT_META_MULTI:
-        return StrategyAdd<Stg_Meta_Multi>(_tfs, _magic_no, _stg);
-      case STRAT_META_REVERSAL:
-        return StrategyAdd<Stg_Meta_Reversal>(_tfs, _magic_no, _stg);
       case STRAT_MFI:
         return StrategyAdd<Stg_MFI>(_tfs, _magic_no, _stg);
       case STRAT_MOMENTUM:
         return StrategyAdd<Stg_Momentum>(_tfs, _magic_no, _stg);
       case STRAT_OBV:
         return StrategyAdd<Stg_OBV>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR:
+         return StrategyAdd<Stg_Oscillator>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR_DIVERGENCE:
+         return StrategyAdd<Stg_Oscillator_Divergence>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR_MULTI:
+         return StrategyAdd<Stg_Oscillator_Multi>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR_CROSS:
+         return StrategyAdd<Stg_Oscillator_Cross>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR_CROSS_SHIFT:
+         return StrategyAdd<Stg_Oscillator_Cross_Shift>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR_CROSS_ZERO:
+         return StrategyAdd<Stg_Oscillator_Cross_Zero>(_tfs, _magic_no, _stg);
+      case STRAT_OSCILLATOR_RANGE:
+         return StrategyAdd<Stg_Oscillator_Range>(_tfs, _magic_no, _stg);
       case STRAT_OSMA:
         return StrategyAdd<Stg_OsMA>(_tfs, _magic_no, _stg);
       case STRAT_PATTERN:
--- src/include/enum.h
+++ src/include/enum.h
@@ -20,48 +20,58 @@
  */
 
 #define ENUM_STRATEGY_DEFINED
-enum ENUM_STRATEGY { // Define list of strategies.
-  STRAT_NONE = 0,    // (None)
-  STRAT_AC,          // AC
-  STRAT_AD,          // AD
-  STRAT_ADX,         // ADX
-  STRAT_ALLIGATOR,   // Alligator
-  STRAT_AMA,         // AMA
-  STRAT_ASI,         // ASI
-  STRAT_ATR,         // ATR
-  STRAT_AWESOME,     // Awesome
-  STRAT_BANDS,       // Bands
-  STRAT_BEARS_POWER, // Bear Power
-  STRAT_BULLS_POWER, // Bulls Power
-  STRAT_BWMFI,       // BWMFI
-  STRAT_CCI,         // CCI
-  STRAT_CHAIKIN,     // Chaikin
-  STRAT_DEMA,        // DEMA
-  STRAT_DEMARKER,    // DeMarker
-  STRAT_ENVELOPES,   // Envelopes
-  STRAT_FORCE,       // Force
-  STRAT_FRACTALS,    // Fractals
-  STRAT_GATOR,       // Gator
-  STRAT_HEIKEN_ASHI, // Heiken Ashi
-  STRAT_ICHIMOKU,    // Ichimoku
-  STRAT_INDICATOR,   // Indicator
-  STRAT_MA,          // MA
-  STRAT_MACD,        // MACD
-  STRAT_META_MIRROR, // (Meta) Mirror
-  STRAT_META_MULTI,  // (Meta) Multi
-  STRAT_META_REVERSAL,// (Meta) Reversal
-  STRAT_MFI,         // MFI
-  STRAT_MOMENTUM,    // Momentum
-  STRAT_OBV,         // OBV
-  STRAT_OSMA,        // OSMA
-  STRAT_PINBAR,      // Pinbar
-  STRAT_PATTERN,     // Pattern
-  STRAT_PIVOT,       // Pivot
-  STRAT_RSI,         // RSI
-  STRAT_RVI,         // RVI
-  STRAT_SAR,         // SAR
-  STRAT_STDDEV,      // StdDev
-  STRAT_STOCHASTIC,  // Stochastic
-  STRAT_WPR,      // WPR
-  STRAT_ZIGZAG,   // ZigZag
+// Defines enum with supported strategy list.
+enum ENUM_STRATEGY {
+  STRAT_NONE = 0,                // (None)
+  STRAT_AC,                      // AC
+  STRAT_AD,                      // AD
+  STRAT_ADX,                     // ADX
+  STRAT_ALLIGATOR,               // Alligator
+  STRAT_AMA,                     // AMA
+  STRAT_ARROWS,                  // Arrows
+  STRAT_ASI,                     // ASI
+  STRAT_ATR,                     // ATR
+  STRAT_AWESOME,                 // Awesome
+  STRAT_BANDS,                   // Bands
+  STRAT_BEARS_POWER,             // Bear Power
+  STRAT_BULLS_POWER,             // Bulls Power
+  STRAT_BWMFI,                   // BWMFI
+  STRAT_CCI,                     // CCI
+  STRAT_CHAIKIN,                 // Chaikin
+  STRAT_DEMA,                    // DEMA
+  STRAT_DEMARKER,                // DeMarker
+  STRAT_ENVELOPES,               // Envelopes
+  STRAT_FORCE,                   // Force
+  STRAT_FRACTALS,                // Fractals
+  STRAT_GATOR,                   // Gator
+  STRAT_HEIKEN_ASHI,             // Heiken Ashi
+  STRAT_ICHIMOKU,                // Ichimoku
+  STRAT_INDICATOR,               // Indicator
+  STRAT_MA,                      // MA
+  STRAT_MA_CROSS_PIVOT,          // MA Cross Pivot
+  STRAT_MA_CROSS_SHIFT,          // MA Cross Shift
+  STRAT_MA_CROSS_SUP_RES,        // MA Cross Sup/Res
+  STRAT_MACD,                    // MACD
+  STRAT_MFI,                     // MFI
+  STRAT_MOMENTUM,                // Momentum
+  STRAT_OBV,                     // OBV
+  STRAT_OSCILLATOR,              // Oscillator
+  STRAT_OSCILLATOR_DIVERGENCE,   // Oscillator
+  STRAT_OSCILLATOR_MULTI,        // Oscillator Multi
+  STRAT_OSCILLATOR_CROSS,        // Oscillator Cross
+  STRAT_OSCILLATOR_CROSS_SHIFT,  // Oscillator Cross Shift
+  STRAT_OSCILLATOR_CROSS_ZERO,   // Oscillator Cross Zero
+  STRAT_OSCILLATOR_RANGE,        // Oscillator Range
+  STRAT_OSMA,                    // OSMA
+  STRAT_PATTERN,                 // Pattern
+  STRAT_PINBAR,                  // Pinbar
+  STRAT_PIVOT,                   // Pivot
+  STRAT_RETRACEMENT,             // Retracement
+  STRAT_RSI,                     // RSI
+  STRAT_RVI,                     // RVI
+  STRAT_SAR,                     // SAR
+  STRAT_STDDEV,                  // StdDev
+  STRAT_STOCHASTIC,              // Stochastic
+  STRAT_WPR,                     // WPR
+  STRAT_ZIGZAG,                  // ZigZag
 };
